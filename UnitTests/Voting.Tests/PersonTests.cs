492e3082e0bb462322fbb912d15be850ebc16830
jensbaetens-odisee <83013151+jensbaetens-odisee@users.noreply.github.com>
2022-02-09 20:05:04 +0100

    einde unit test

--- UnitTests/Voting.Tests/PersonTests.cs
+++ UnitTests/Voting.Tests/PersonTests.cs
@@ -10,10 +10,53 @@ namespace Voting.Tests
     [TestFixture]
     internal class PersonTests
     {
-        [Test]
-        public void CanVote_WithCorrectAge_ReturnsTrue()
-        {
+        /* [TestCase(18, "Jens")]
+         [TestCase(23, "Jan")]
+         [TestCase(19, "Piet")]
+         [TestCase(64, "Pol")]
+         public void CanVote_WithCorrectAge_ReturnsTrue(int age, string name)
+         {
+             //Arrange
+             Person person = new Person(age);
 
+             //Act
+             bool result = person.CanVote();
+
+             //Assert
+             Assert.True(result);
+         }
+
+         [TestCase(4)]
+         [TestCase(204)]
+         public void CanVote_WithIncorrectAge_ReturnsFalse(int age)
+         {
+             //Arrange
+             Person person = new Person(age);
+
+             //Act
+             bool result = person.CanVote();
+
+             //Assert
+             Assert.False(result);
+         }
+        */
+        [TestCase(18, ExpectedResult =true)]
+        [TestCase(23, ExpectedResult = true)]
+        [TestCase(19, ExpectedResult = true)]
+        [TestCase(64, ExpectedResult = true)]
+        [TestCase(4, ExpectedResult = false)]
+        [TestCase(204, ExpectedResult = false)]
+        public bool CanVote_WithVariousAges_ReturnsFalseWhenLessThan18OrMoreThan130(int age)
+        {
+            //Arrange
+            Person person = new Person(age);
+
+            //Act
+            bool result = person.CanVote();
+
+            //Assert
+            return result; // return result, wordt gecontroleerd met de expectedResult in de testcase
+            // let op het return type moet correct zijn (geen void)
         }
     }
 }
