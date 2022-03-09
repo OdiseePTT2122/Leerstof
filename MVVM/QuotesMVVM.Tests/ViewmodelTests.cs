a900a3071888ec1ffe4591367466b954a1345c0c
jensbaetens-odisee <83013151+jensbaetens-odisee@users.noreply.github.com>
2022-03-09 18:46:12 +0100

    slides en temporary code

---
+++ MVVM/QuotesMVVM.Tests/ViewmodelTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotesMVVM.Tests
+{
+    [TestFixture]
+    internal class ViewmodelTests
+    {
+        [Test]
+        public void SelectedQuote_ItemSelected_AuteurEnTekstAangepast()
+        {
+            // Arrange
+            Viewmodel sut = new Viewmodel();
+            Quote quote = new Quote("tekst", "auteur");
+
+            // Act
+            sut.SelectedQuote = quote;
+
+            // Assert
+            Assert.That(sut.Auteur, Is.EqualTo("auteur"));
+            Assert.That(sut.Tekst, Is.EqualTo("tekst"));
+        }
+
+        [Test]
+        public void Auteur_Changed_NotifyEventSend()
+        {
+            // Arrange
+            Viewmodel sut = new Viewmodel();
+            string property = "";
+            // dit is onze eigen event handler
+            sut.PropertyChanged += (sender, eventargs) =>
+            {
+                property = eventargs.PropertyName; 
+            };
+
+            // Act
+            sut.Auteur = "auteur";
+
+            // Assert
+            Assert.That(property, Is.EqualTo("Auteur"));
+        }
+
+        [Test]
+        public void Tekst_Changed_NotifyEventSend()
+        {
+            // Arrange
+            Viewmodel sut = new Viewmodel();
+            string property = "";
+            // dit is onze eigen event handler
+            sut.PropertyChanged += (sender, eventargs) =>
+            {
+                property = eventargs.PropertyName;
+            };
+
+            // Act
+            sut.Tekst = "auteur";
+
+            // Assert
+            Assert.That(property, Is.EqualTo("Tekst"));
+        }
+
+        [Test]
+        public void SaveQuoteCommand_WithAuthorEnTekstIngevuld_AddNewItem()
+        {
+            // Arrange
+            Viewmodel sut = new Viewmodel();
+            sut.Auteur = "auteur";
+            sut.Tekst = "tekst";
+
+            // Act
+            sut.SaveQuoteCommand.Execute(null);
+
+            // Assert
+            Assert.That(sut.Quotes.Count, Is.EqualTo(1));
+            Assert.That(sut.Quotes[0].Text, Is.EqualTo("tekst"));
+            Assert.That(sut.Quotes[0].Author, Is.EqualTo("auteur"));
+        }
+    }
+}
--- MVVM/QuotesMVVM/Viewmodel.cs
+++ MVVM/QuotesMVVM/Viewmodel.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace QuotesMVVM
 {
+    // Uitgevoerde stappen
+    // 1. Aanmaken deze klasse en connecteer het vanuit het Mainwindow.xaml.cs
+    // 2. Verzorg databinding tussen UI-elementen en public properties in het Viewmodel
+    // 3. Bij het drukken op knoppen, etc worden commando's verstuurd
+        // 3.A. Command binden met het commando in het viewmodel
+        // 3.B. RelayCommand klasse maken (implementeert ICommand / gedeeld door alle Command-klassen)
+        // 3.C. Voor elke UI-element commando verstuurt maak je zo een Command aan (argument is de functie die het event afhandeld)
+    // 4. Zorg ervoor dat de UI notificaties krijgt om zijn updates door te sturen
+        // 4.A. List -> Observable Collection
+        // 4.B. Update inhoud van de textboxjes als de waarde van de public properties aangepast wordt.
+
     public class Viewmodel: INotifyPropertyChanged
     {
         private string auteur = "";
         public string Auteur { get => auteur; set
             {
                 auteur = value;
-                PropertyChanged(this, new PropertyChangedEventArgs("Auteur"));
+                //PropertyChanged(this, new PropertyChangedEventArgs("Auteur"));
+                OnPropertyChanged();
             }
         }
 
@@ -23,7 +36,8 @@ namespace QuotesMVVM
         public string Tekst { get => tekst; set
             {
                 tekst = value;
-                PropertyChanged(this, new PropertyChangedEventArgs("Tekst"));
+                //PropertyChanged(this, new PropertyChangedEventArgs("Tekst"));
+                OnPropertyChanged();
             }
         }
 
@@ -57,6 +71,15 @@ namespace QuotesMVVM
         {
             Auteur = quote.Author;
             Tekst = quote.Text;
+        }
+
+        // CallerMemberName -> hier wordt automatisch de naam van de functie die het oproept ingeplaatst
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
