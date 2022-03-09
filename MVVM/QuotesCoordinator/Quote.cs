846c62689f7888af4784babbbfded17b497d81e2
jensbaetens-odisee <83013151+jensbaetens-odisee@users.noreply.github.com>
2022-03-09 18:53:05 +0100

    coordinator start

---
+++ MVVM/QuotesCoordinator/Quote.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quotes.MVVMC
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+
+        public Quote(string text, string author)
+        {
+            Text = text;
+            Author = author;
+        }
+
+        public Quote()
+        {
+
+        }
+
+            public override bool Equals(object obj)
+        {
+            if(obj is Quote)
+            {
+                Quote quote = (Quote)obj;
+                return quote.Author == Author && quote.Text == Text;
+            }
+            return false;
+        }
+    }
+}
---
+++ MVVM/QuotesCoordinator/QuoteDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quotes.MVVMC
+{
+    public class QuoteDbContext: DbContext
+    {
+        public QuoteDbContext():base("quotes")
+        {
+           // Database.SetInitializer(new MigrateDatabaseToLatestVersion<QuoteDbContext, Configuration>());
+        }
+
+
+        public virtual DbSet<Quote> Quotes { get; set; }
+    }
+}
